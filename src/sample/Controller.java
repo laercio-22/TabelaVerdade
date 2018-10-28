435b331f11dbcf5039551333cb138ff087141e31
laercio22 <laercioneto22@gmail.com>
2018-10-28 19:42:30 -0300

    Layout pronto e funcional

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,71 @@
 package sample;
 
-public class Controller {
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Controller{
+
+    @FXML
+    private TextField campo;
+
+    @FXML
+    public void p (){
+        campo.setText(campo.getText()+" P");
+    }
+
+    @FXML
+    public void q (){
+        campo.setText(campo.getText()+" Q");
+    }
+
+    @FXML
+    public void r (){
+        campo.setText(campo.getText()+" R");
+    }
+
+    @FXML
+    public void s (){
+        campo.setText(campo.getText()+" S");
+    }
+
+    @FXML
+    public void negacao (){
+        campo.setText(campo.getText()+" ~");
+    }
+
+    @FXML
+    public void implicacao (){
+        campo.setText(campo.getText()+" ->");
+    }
+
+    @FXML
+    public void seSomenteSe (){
+        campo.setText(campo.getText()+" <->");
+    }
+
+    @FXML
+    public void se (){
+        campo.setText(campo.getText()+" <->");
+    }
+
+    @FXML
+    public void ouOu (){
+        campo.setText(campo.getText()+" !");
+    }
+
+    @FXML
+    public void e (){
+        campo.setText(campo.getText()+" ^");
+    }
+
+    @FXML
+    public void ou (){
+        campo.setText(campo.getText()+" ou");
+    }
+
+    @FXML
+    public void reset (){
+        campo.setText(" ");
+    }
 }
