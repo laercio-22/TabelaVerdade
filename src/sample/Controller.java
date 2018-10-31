ef3f8b0311b6908df32734c79908a20c2acf883d
Ravi Assis <ravi.assis@dtidigital.com.br>
2018-10-31 11:42:39 -0300

    Add pacote calculo com classe responspavel pelo calculo - resultado impresso na tela

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,18 @@
 package sample;
 
 
+import calculo.Logico;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class Controller{
 
     @FXML
     private TextField campo;
+    
+    @FXML
+    private TextArea resultado;
 
 
     @FXML
@@ -22,70 +27,80 @@ public class Controller{
 
     @FXML
     public void p (){
-        campo.setText(campo.getText()+" P");
+        campo.setText(campo.getText()+"P ");
     }
 
     @FXML
     public void q (){
-        campo.setText(campo.getText()+" Q");
+        campo.setText(campo.getText()+"Q ");
     }
 
     @FXML
     public void r (){
-        campo.setText(campo.getText()+" R");
+        campo.setText(campo.getText()+"R ");
     }
 
     @FXML
     public void s (){
-        campo.setText(campo.getText()+" S");
+        campo.setText(campo.getText()+"S ");
     }
 
     @FXML
     public void negacao (){
-        campo.setText(campo.getText()+" ~");
+        campo.setText(campo.getText()+"~");
     }
 
     @FXML
     public void implicacao (){
-        campo.setText(campo.getText()+" ->");
+        campo.setText(campo.getText()+"-> ");
     }
 
     @FXML
     public void seSomenteSe (){
-        campo.setText(campo.getText()+" <->");
+        campo.setText(campo.getText()+"<-> ");
     }
 
     @FXML
     public void se (){
-        campo.setText(campo.getText()+" ->");
+        campo.setText(campo.getText()+"-> ");
     }
 
     @FXML
     public void ouOu (){
-        campo.setText(campo.getText()+" !");
+        campo.setText(campo.getText()+"! ");
     }
 
     @FXML
     public void e (){
-        campo.setText(campo.getText()+" ^");
+        campo.setText(campo.getText()+"^ ");
     }
 
     @FXML
     public void ou (){
-        campo.setText(campo.getText()+" v");
+        campo.setText(campo.getText()+"v ");
     }
 
     @FXML
     public void reset (){
-        campo.setText(" ");
+        campo.setText("");
     }
 
 
     @FXML
     public void calcular (){
 
-        InserirValores.valores(campo.getText());
-
+        //InserirValores.valores(campo.getText());
+    	String [][] tabela = Logico.calcular(campo.getText());
+    	String s = "";
+    	for(int i = 0; i < tabela.length; i++) {
+    		for (int j = 0; j < tabela[i].length; j++) {
+    			if (j == 0) s += "|";
+				s += " " + tabela[i][j] + " |";
+    		}
+    		s += "\n";
+    	}
+    	
+    	resultado.setText(s);
     }
 
 }
